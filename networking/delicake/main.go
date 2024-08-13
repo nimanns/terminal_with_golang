37b1193d813502b4978a9a97120f457de0c01ca7
Nima Niazi <qwerty.nma@gmail.com>
2024-08-12 22:56:26 -0400

    delicake: add migrations.

--- networking/delicake/main.go
+++ networking/delicake/main.go
@@ -3,14 +3,22 @@ package main
 import (
 	"log"
 
+	"delicake/models"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
 
 func main() {
-	_, err := gorm.Open(sqlite.Open("delicake.db"), &gorm.Config{})
+	db, err := gorm.Open(sqlite.Open("delicake.db"), &gorm.Config{})
 	if err != nil {
 		log.Fatal("Failed to connect to database:", err)
 	}
 	log.Println("Connected to database successfully")
+
+	// migrations
+	err = db.AutoMigrate(&models.Cake{}, &models.Order{})
+	if err != nil {
+		log.Fatal("Failed to perform migrations:", err)
+	}
+	log.Println("Migrations completed successfully")
 }

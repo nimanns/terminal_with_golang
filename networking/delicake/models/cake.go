20c7a031bebf1416bdeed0e9a661886d9e5b6f21
Nima Niazi <qwerty.nma@gmail.com>
2024-08-14 23:23:55 -0400

    delicake: add input validation to Cake and Order models

--- networking/delicake/models/cake.go
+++ networking/delicake/models/cake.go
@@ -1,11 +1,12 @@
 package models
 
-import "gorm.io/gorm"
+import (
+	"gorm.io/gorm"
+)
 
 type Cake struct {
 	gorm.Model
-	Name        string
-	Description string
-	Price       float64
+	Name        string  `json:"name" binding:"required,min=2,max=100"`
+	Description string  `json:"description" binding:"max=500"`
+	Price       float64 `json:"price" binding:"required,min=0"`
 }
-
--- networking/delicake/models/order.go
+++ networking/delicake/models/order.go
@@ -1,11 +1,14 @@
 package models
 
-import "gorm.io/gorm"
+import (
+	"gorm.io/gorm"
+)
 
 type Order struct {
 	gorm.Model
-	CustomerName string
-	CakeID       uint
-	Cake         Cake
-	Quantity     int
+	CustomerName string  `json:"customer_name" binding:"required,min=2,max=100"`
+	CakeID       uint    `json:"cake_id" binding:"required"`
+	Cake         Cake    `json:"cake"`
+	Quantity     int     `json:"quantity" binding:"required,min=1"`
+	TotalPrice   float64 `json:"total_price"`
 }
